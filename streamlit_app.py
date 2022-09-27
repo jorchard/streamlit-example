5d5b6c1f276580fbbc05b72938cf857b8084ad3d
Jeff Orchard <jorchard@uwaterloo.ca>
2022-09-26 23:54:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,8 @@ with st.echo(code_location='below'):
     Point = namedtuple('Point', 'x y')
     data = []
 
+    print('Hi Jeff')
+    
     points_per_turn = total_points / num_turns
 
     for curr_point_num in range(total_points):
